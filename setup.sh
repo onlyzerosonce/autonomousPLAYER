5aa6ec064e7a07a41b59e0a5ca04f13b93cb2ab0
V <vijaykumar14641@gmail.com>
2019-11-11 13:55:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+cd .. 
 mv -f autonomousPLAYER/awesome .config/
 mv -f autonomousPLAYER/Thunar .config/
 mv -f autonomousPLAYER/terminator .config/
 mv -f autonomousPLAYER/.bashrc "$HOME"
 mv -f autonomousPLAYER/.bash_aliases "$HOME"
+rm -rf autonomousPLAYER
+rm -rf autonomousPLAYER
+rm -rf autonomousPLAYER
