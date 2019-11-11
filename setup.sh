29f02e627ce63acda197f924608f34a3bd1d7a53
V <vijaykumar14641@gmail.com>
2019-11-11 14:14:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ cp -rf autonomousPLAYER/Thunar .config/
 cp -rf autonomousPLAYER/terminator .config/
 cp -f autonomousPLAYER/.bashrc "$HOME"
 cp -f autonomousPLAYER/.bash_aliases "$HOME"
+[ -f .readingstats ] && echo "Pages|Date|FileName">.readingstats
 
